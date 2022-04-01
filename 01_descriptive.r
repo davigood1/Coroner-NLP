62fccc781642e6e5180c9377cd9ae822c79b9720
David Goodman-Meza <69010189+davigood1@users.noreply.github.com>
2022-04-01 07:53:26 -0700

    Update 01_descriptive.r
    Add license information.

--- 01_descriptive.r
+++ 01_descriptive.r
@@ -1,3 +1,9 @@
+Copyright (c) 2022, David Goodman-Meza
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. 
+
 # install.packages("pacman")
 library(pacman)
 p_load(tidyverse, tidylog, purrr, colorspace, flextable) # Basic
